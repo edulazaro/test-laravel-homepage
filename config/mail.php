1e69d865d898be189bca71d4debd6bdaf31ba9e2
Edu Lázaro <eduzro@gmail.com>
2021-06-16 19:58:44 +0200

    js fin

--- config/mail.php
+++ config/mail.php
@@ -51,9 +51,6 @@ return [
 
         'mailgun' => [
             'transport' => 'mailgun',
-            'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
-            'domain' => env('MAILGUN_DOMAIN'),
-            'secret' => env('MAILGUN_SECRET'),
         ],
 
         'postmark' => [
--- resources/views/emails/subscribed.blade.php
+++ resources/views/emails/subscribed.blade.php
@@ -1,3 +1,16 @@
 <body>
-    Thank you for subscribing to Horse and Country
+
 </body>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Horese and Country</title>
+</head>
+<body>
+    <h1>Subscribred!</h1>
+    <p>Thank you for subscribing to Horse and Country</p>
+   
+    <p>Have a good day</p>
+</body>
+</html>
